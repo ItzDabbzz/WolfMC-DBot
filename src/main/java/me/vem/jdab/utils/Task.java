275a6672577260e75b991c6df8cd918176799724
ItzDabbzz <ItzDabbzz@gmail.com>
2019-12-26 11:13:00 -0600

    Revert "First Bot Upload"
    This reverts commit d3a46da0d7b76c64ba90cc4dbada523f312deb2c.

--- src/main/java/me/vem/jdab/utils/Task.java
+++
@@ -1,9 +0,0 @@
-package me.vem.jdab.utils;
-
-import java.util.TimerTask;
-
-public class Task extends TimerTask {
-	private Runnable func;
-	public Task(Runnable r) { this.func = r; }
-	@Override public void run() { func.run(); }
-}
